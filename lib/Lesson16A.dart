9c1d4a87e07bd580e62ee38d60b60dd9f7878da0
Keisuke2162 <applabo.kei@gmail.com>
2021-12-26 15:19:39 +0900

    Lesson16実装準備

---
+++ lib/Lesson16A.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class Lesson16A extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lesson16A"),
+      ),
+      body: Center(
+        child: Text("16A"),
+      ),
+    );
+  }
+}
