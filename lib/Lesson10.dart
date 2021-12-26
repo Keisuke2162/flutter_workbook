442d535f1b967e5fc45343f96cbb45d3c87a79da
Keisuke2162 <applabo.kei@gmail.com>
2021-12-26 13:29:53 +0900

    Lesson10実装

---
+++ lib/Lesson10.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+// １０：PAGE_VIEWで画面表示(難易度C)
+
+class Lesson10 extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final PageController controller = PageController(initialPage: 0);
+
+    return Scaffold(
+      body: PageView(
+        scrollDirection: Axis.horizontal,
+        controller: controller,
+        children: <Widget>[
+          // Page1
+          Center(
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.blueAccent,
+              child: Center(
+                child: Text("1"),
+              ),
+            ),
+          ),
+
+          // Page2
+          Center(
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.yellowAccent,
+              child: Center(
+                child: Text("2"),
+              ),
+            ),
+          ),
+
+          // Page3
+          Center(
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.redAccent,
+              child: Center(
+                child: Text("3"),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_workbook/Lesson1.dart';
+import 'package:flutter_workbook/Lesson10.dart';
 import 'package:flutter_workbook/Lesson2.dart';
 import 'package:flutter_workbook/Lesson3.dart';
 import 'package:flutter_workbook/Lesson4.dart';
@@ -23,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Lesson9(),
+      home: Lesson10(),
     );
   }
 }
