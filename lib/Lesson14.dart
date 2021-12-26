cd987db85bd298caeb787002a419aad70f7fbab6
Keisuke2162 <applabo.kei@gmail.com>
2021-12-26 14:23:54 +0900

    Lesson14実装

---
+++ lib/Lesson14.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Lesson14 extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lesson14"),
+      ),
+      body: Center(
+        child: Text("Lesson14 はアプリアイコン変更課題！"),
+      ),
+    );
+  }
+}
